7f1ce17ac09fb64524035655f07805fd1752d4ac
Mickael Moreau <mickael.moreau.pro@gmail.com>
2018-12-11 12:44:03 +0100

    Trying to integrate emailjs. Still some issue when running code...

--- docs/bibliographie.md
+++ docs/bibliographie.md
@@ -8,9 +8,26 @@
 - https://developers.google.com/gmail/api/downloads
 - https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/gapi.client.gmail
 - https://www.npmjs.com/package/googleapis
+- https://www.npmjs.com/package/emailjs-imap-handler
+- https://emailjs.org/
+- http://tphangout.com/angular-2-sending-mails-from-your-app/
+- https://github.com/microsoft/typescript/issues/3019
+- https://github.com/Microsoft/TypeScript/issues/15031
+- http://definitelytyped.org/
+- https://github.com/teppeis/closure-ts
+- https://www.stevefenton.co.uk/2013/01/complex-typescript-definitions-made-easy/
+
+- https://github.com/emailjs/emailjs-tcp-socket/issues/31
+- https://github.com/webpack-contrib/imports-loader#disable-amd
+- https://github.com/Axel186/emailjs-and-webpack-example/blob/master/webpack.config.js
+- https://github.com/emailjs/emailjs-imap-client/blob/master/src/client.js
+
+- https://github.com/webpack-contrib/polymer-webpack-loader
+- https://github.com/vuejs/vue-loader
+- https://github.com/TheLarkInn/angular2-template-loader
 
 ```bash
 # Added packages :
-yarn add emailjs-imap-client
+yarn add emailjs-imap-client imports-loader
 
 ```
--- src/app/moon-box/components/box-reader/box-reader.component.ts
+++ src/app/moon-box/components/box-reader/box-reader.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+// declare module 'emailjs-imap-client/*'{
+//   var _a: any;
+//   export = _a;
+// }
+// import ImapClient from 'emailjs-imap-client';
+// import ImapClient from 'emailjs-imap-client/src/client.js';
+// No types availables yet, using webpack require ways :
+
+// declare const ImapClient: any;
+declare const require: any; // To avoid typeScript error about require that don't exist since it's webpack level
+// const ImapClient:any = require('emailjs-imap-client/dist/client.js');
+// const ImapClient: any = require('imports-loader?define=>false!emailjs-imap-client');
+const ImapClient: any = require('imports-loader?define=>false!emailjs-imap-client/src/client.js');
 
 @Component({
   selector: 'app-box-reader',
@@ -6,7 +19,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./box-reader.component.scss']
 })
 export class BoxReaderComponent implements OnInit {
-  constructor() {}
+  imapProviders = {
+    OVH: {
+      name: 'O.V.H.',
+      serverUrl: 'SSL0.OVH.NET',
+      serverPort: '993'
+    },
+    GoDaddy: {
+      name: 'GoDaddy',
+      serverUrl: 'imap.secureserver.net',
+      serverPort: '993'
+    },
+    LWS: {
+      name: 'L.W.S.',
+      serverUrl: 'mail07.lwspanel.com',
+      serverPort: '993'
+    }
+  };
+  defaultProvider = 'GoDaddy';
+  imapClient: any = null;
+
+  constructor() {
+    const imapProvider = this.imapProviders[this.defaultProvider];
+    const ctx = {
+      auth: {
+        user: 'testuser',
+        pass: 'testpass'
+      }
+    };
+
+    this.imapClient = new ImapClient(imapProvider.serverUrl, imapProvider.serverPort, ctx);
+    this.imapClient.onerror = function(error: any) {
+      console.error(error);
+    };
+    this.imapClient.onerror = function(error: any) {
+      console.error(error);
+    };
+    this.imapClient.connect().then(() => {
+      console.log('Connected with context : ', ctx);
+      this.imapClient.listMailboxes().then((mailboxes: any) => {
+        console.log('Having mailboxes : ', mailboxes);
+
+        this.imapClient.logout().then(() => {
+          /* connection terminated for Imap service */
+          console.log('Logging out of : ', ctx);
+          this.imapClient.close().then(() => {
+            /* connection terminated */
+            console.log('Closing connexions for : ', ctx);
+          });
+        });
+      });
+    });
+  }
 
   ngOnInit() {}
 }
