751900bebe2a6c29ba60bdbf77d9cf5188bb6363
Mickael Moreau <mickael.moreau.pro@gmail.com>
2018-12-22 04:23:33 +0100

    Preparing frontend for Production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,7 +15,7 @@ export const environment = {
   moonBoxBackendBasePath: '/',
   defaultLanguage: 'fr-FR',
   supportedLanguages: ['fr-FR', 'en-US'],
-  clientSecret: 'adx79c5n3swfous'
+  clientSecret: 'ameuioph79c9n96'
 };
 /*
  * For easier debugging in development mode, you can import the following file
