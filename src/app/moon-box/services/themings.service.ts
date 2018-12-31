02ce179028703363ca02e3739dc68787d92d9948
Mickael Moreau <mickael.moreau.pro@gmail.com>
2018-12-31 21:07:42 +0100

    Improving dark theme

--- src/app/moon-box/services/themings.service.ts
+++ src/app/moon-box/services/themings.service.ts
@@ -18,13 +18,17 @@ export class ThemingsService {
   };
   currentThemeClassInjectionSubject = new BehaviorSubject<string>(this.currentTheme);
 
-  constructor(private localStorage: LocalStorage) {}
+  constructor(private localStorage: LocalStorage) {
+    this.localStorage.getItem<string>('current-theme').subscribe((storedTheme: string) => {
+      this.setTheme(storedTheme || this.currentTheme);
+    });
+  }
 
   async setTheme(theme: string) {
     logReview.debug('Setting theme to : ', theme);
     this.currentTheme = theme;
     this.currentThemeClassInjectionSubject.next(this.baseThemesClassInjections[this.currentTheme] || '');
-    return await this.localStorage.setItem('current-theme', theme);
+    return await this.localStorage.setItem('current-theme', theme).toPromise();
   }
 
   getCurrentThemeClassInjections() {
